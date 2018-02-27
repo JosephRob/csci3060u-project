e74860c47704ae70133af42d6080546962414643
JosephRob <joseph.robertson1@uoit.net>
2018-02-27 13:41:11 -0500

    think test done

--- project/test.sh
+++ project/test.sh
@@ -2,6 +2,9 @@
 declare -i place
 
 make
+mkdir filesTemp
+cp files/* filesTemp -r
+rm files/dailyUpdate.txt
 
 for file in ./tests/*
 do
@@ -14,10 +17,15 @@ do
             #echo $place
             echo $test
             ./theProgram < $test > $file"/outputs/out"$place".txt"
-            rm $file/outputs/asdf$place.txt
+            diff -q $file/outputs/out$place.txt $file/expectedOutputs/out$place.txt
             place=$place+1
         done
+        rm -r $file/outputs
     fi
 done
 
+diff -q files/dailyUpdate.txt tests/expectedDay.txt
+
 make clean
+cp filesTemp/* files
+rm filesTemp -r
