a4be47eb51e7366f5b64170435a0d6d38d24a65e
JosephRob <joseph.robertson1@uoit.net>
2018-02-26 19:30:06 -0500

    output exists

--- project/test.sh
+++ project/test.sh
@@ -1,15 +1,21 @@
 #./theProgram < asdf.txt > qwerty.txt
+declare -i place
+
 make
+
 for file in ./tests/*
 do
     if [[ -d $file ]]; then
         #echo $file
+        place=1
+        mkdir $file/outputs -p
         for test in $file/inputs/*
         do
+            #echo $place
             echo $test
-            ./theProgram < $test > asdf.txt
+            ./theProgram < $test > $file"/outputs/asdf"$place".txt"
+            place=$place+1
         done
     fi
 done
-rm asdf.txt
 make clean
