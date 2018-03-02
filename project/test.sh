0ba7387fc63119bdeb646b924e564d53440116b9
JosephRob <joseph.robertson1@uoit.net>
2018-03-02 15:26:17 -0500

    added some to test fixed daily case

--- project/test.sh
+++ project/test.sh
@@ -1,11 +1,11 @@
 #./theProgram < asdf.txt > qwerty.txt
 declare -i place
-
+declare -i fails
 make
 mkdir filesTemp
 cp files/* filesTemp -r
 rm files/dailyUpdate.txt
-
+fails=0
 for file in ./tests/*
 do
     if [[ -d $file ]]; then
@@ -18,14 +18,21 @@ do
             echo $test
             ./auctionHouse < $test > $file"/outputs/out"$place".txt"
             diff -q $file/outputs/out$place.txt $file/expectedOutputs/out$place.txt
+            
+            if [ "$?" -ne "0" ]; then
+                fails=$fails+1
+            fi
+
             place=$place+1
         done
         rm -r $file/outputs
     fi
 done
 
+echo "$fails test(s) failed"
+
 diff -q files/dailyUpdate.txt tests/expectedDay.txt
 
-make clean      
-cp filesTemp/* files
-rm filesTemp -r
+#make clean      
+#cp filesTemp/* files
+#m filesTemp -r
